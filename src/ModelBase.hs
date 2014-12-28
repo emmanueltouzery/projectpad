36b26b7a02dbefb8a3e860656d4f0ecb8e6fa772
Emmanuel Touzery <etouzery@gmail.com>
2014-12-28 20:35:36 +0100

    haskell code to add servers, not called by QML yet

--- src/ModelBase.hs
+++ src/ModelBase.hs
@@ -1,16 +1,18 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, DeriveDataTypeable #-}
 module ModelBase where
 
 import Database.Persist.TH
+import Data.Text (Text)
+import Data.Typeable
 
-type Password = String
-type IpAddress = String
+type Password = Text
+type IpAddress = Text
 
 data InterestType = PoiApplication | PoiLogFile | PoiDataFile
-	deriving (Show, Read, Eq)
+	deriving (Show, Read, Eq, Typeable)
 derivePersistField "InterestType"
 
 -- app would be bug tracking, CI...
 data ServerType = SrvSsh | SrvRdp | SrvDatabase | SrvApplication
-	deriving (Show, Read, Eq)
+	deriving (Show, Read, Eq, Typeable)
 derivePersistField "ServerType"
--- src/ProjectView.hs
+++ src/ProjectView.hs
@@ -1,13 +1,18 @@
-{-# LANGUAGE OverloadedStrings, DeriveDataTypeable, TypeFamilies #-}
+{-# LANGUAGE OverloadedStrings, DeriveDataTypeable, TypeFamilies, ViewPatterns #-}
 module ProjectView where
 
 import Control.Applicative
 import Control.Concurrent.MVar
 import Graphics.QML
 import Database.Esqueleto
+import qualified Database.Persist as P
 import Data.Typeable
 import Control.Monad
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Maybe
 
+import ModelBase
 import Model
 
 data ProjectViewState = ProjectViewState
@@ -22,8 +27,9 @@ readServers projectId = select $ from $ \s -> do
 	orderBy [asc (s ^. ServerDesc)]
 	return s
 
-updateProjectViewCache :: SqlBackend -> ProjectViewState -> Int -> IO ()
-updateProjectViewCache sqlBackend state projectId = do
+updateProjectViewCache :: SqlBackend -> ObjRef ProjectViewState -> Int -> IO ()
+updateProjectViewCache sqlBackend state_ projectId = do
+	let state = fromObjRef state_
 	newServers <- runSqlBackend sqlBackend (readServers projectId)
 	let newQmlServers = mapM newObjectDC newServers
 	modifyMVar_ (curProjectId state) $ const (return $ Just projectId)
@@ -35,8 +41,24 @@ getServers sqlBackend _state projectId = do
 	putStrLn "getServers() called!"
 	let state = fromObjRef _state
 	pId <- readMVar (curProjectId state)
-	when (not $ pId == Just projectId) (updateProjectViewCache sqlBackend state projectId)
+	when (not $ pId == Just projectId) $
+		updateProjectViewCache sqlBackend _state projectId
 	readMVar $ servers state
+
+getCurProject :: ObjRef ProjectViewState -> IO Int
+getCurProject (fromObjRef -> state) = fromMaybe (error "No current project!")
+	<$> readMVar (curProjectId state)
+
+addServer :: SqlBackend -> ObjRef ProjectViewState
+	-> Text -> IpAddress -> Text -> Text -> Text -> IO ()
+addServer sqlBackend stateRef
+	sDesc ipAddr username password serverTypeT = do
+	pId <- getCurProject stateRef
+	let pidKey = toSqlKey $ fromIntegral pId
+	let srvType = read $ T.unpack serverTypeT
+	let server = Server sDesc ipAddr username password srvType pidKey
+	runSqlBackend sqlBackend $ P.insert server
+	updateProjectViewCache sqlBackend stateRef pId
 
 createProjectViewState :: SqlBackend -> IO (ObjRef ProjectViewState)
 createProjectViewState sqlBackend = do
@@ -45,6 +67,7 @@ createProjectViewState sqlBackend = do
 		<*> newMVar []
 	projectViewClass <- newClass
 		[
-			defMethod "getServers" (getServers sqlBackend)
+			defMethod "getServers" (getServers sqlBackend),
+			defMethod "addServer" (addServer sqlBackend)
 		]
 	newObject projectViewClass projectViewState
