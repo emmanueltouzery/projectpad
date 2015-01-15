cf24f7cae9d6c7b2aedc7edca0c07c9bc429c84e
Emmanuel Touzery <etouzery@gmail.com>
2015-01-15 17:34:52 +0100

    icon for applications

--- qml/poiactions.js
+++ qml/poiactions.js
@@ -1,4 +1,5 @@
 var actions = {
 	"PoiLogFile": {"text": "Open", "icon": "glyphicons-30-notes-2"},
-	"PoiCommandToRun": {"text": "Run", "icon": "glyphicons-138-cogwheels"}
+	"PoiCommandToRun": {"text": "Run", "icon": "glyphicons-138-cogwheels"},
+	"PoiApplication":{"text": "Open", "icon": "glyphicons-149-folder-flag"}
 }
