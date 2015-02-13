a622b8292244712dd10de6ffe38e3b4797baaa74
Emmanuel Touzery <etouzery@gmail.com>
2015-02-13 06:01:19 +0100

    icon for configuration file POI

--- qml/poiactions.js
+++ qml/poiactions.js
@@ -1,5 +1,6 @@
 var actions = {
 	"PoiLogFile": {"text": "Open", "icon": "glyphicons-30-notes-2"},
 	"PoiCommandToRun": {"text": "Run", "icon": "glyphicons-138-cogwheels"},
-	"PoiApplication":{"text": "Open", "icon": "glyphicons-149-folder-flag"}
+	"PoiApplication":{"text": "Open", "icon": "glyphicons-149-folder-flag"},
+	"PoiConfigFile":{"text": "Open", "icon": "glyphicons-281-settings"}
 }
