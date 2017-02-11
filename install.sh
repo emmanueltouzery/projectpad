6ca41408b293709ebe841fa7113bc6f9e19b1450
Emmanuel Touzery <etouzery@gmail.com>
2017-02-11 11:09:08 +0100

    move from stackage lts 5.2 to 7.19, basically 1 year newer

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 command -v patch >/dev/null 2>&1 || { echo >&2 "I require the command 'patch' but it's not installed.  Aborting."; exit 1; }
 
-if [ ! -d "persistent-sqlcipher-2.2" ]; then
+if [ ! -d "persistent-sqlcipher-2.6" ]; then
     echo "setting up persistent sqlcipher integration..."
-    wget https://hackage.haskell.org/package/persistent-sqlite-2.2/persistent-sqlite-2.2.tar.gz
-    tar xvfz persistent-sqlite-2.2.tar.gz
-    rm persistent-sqlite-2.2.tar.gz
+    wget https://hackage.haskell.org/package/persistent-sqlite-2.6/persistent-sqlite-2.6.tar.gz
+    tar xvfz persistent-sqlite-2.6.tar.gz
+    rm persistent-sqlite-2.6.tar.gz
     patch -p0 <<'EOF'
---- persistent-sqlite-2.2/persistent-sqlite.cabal	2016-02-17 19:47:29.109281623 +0100
-HsColour+++ persistent-sqlite-2.2/persistent-sqlcipher.cabal	2016-02-17 19:48:26.711577826 +0100
+--- persistent-sqlite-2.6/persistent-sqlite.cabal	2016-08-14 00:03:34.000000000 +0200
++++ persistent-sqlite-2.6/persistent-sqlcipher.cabal	2017-02-11 10:53:17.086611649 +0100
 @@ -1,4 +1,4 @@
 -name:            persistent-sqlite
 +name:            persistent-sqlcipher
- version:         2.2
+ version:         2.6
  license:         MIT
  license-file:    LICENSE
-@@ -38,12 +38,7 @@
+@@ -39,12 +39,7 @@
      exposed-modules: Database.Sqlite
                       Database.Persist.Sqlite
      ghc-options:     -Wall
@@ -23,14 +23,14 @@ HsColour+++ persistent-sqlite-2.2/persistent-sqlcipher.cabal	2016-02-17 19:48:26
 -    else
 -        c-sources:   cbits/sqlite3.c
 -        include-dirs: cbits
--        cc-options:  -fPIC
+-        cc-options:  -fPIC -std=c99
 +    extra-libraries: sqlcipher
 
      c-sources: cbits/config.c
 
 EOF
-    mv persistent-sqlite-2.2/ persistent-sqlcipher-2.2/
-    mv persistent-sqlcipher-2.2/persistent-sqlite.cabal persistent-sqlcipher-2.2/persistent-sqlcipher.cabal
+    mv persistent-sqlite-2.6/ persistent-sqlcipher-2.6/
+    mv persistent-sqlcipher-2.6/persistent-sqlite.cabal persistent-sqlcipher-2.6/persistent-sqlcipher.cabal
 fi
 echo "Will now install the haskell compiler if needed"
 stack setup
