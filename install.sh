6dacd10d18a6ea91d05a33727c0136c032c1d675
Emmanuel Touzery <etouzery@gmail.com>
2018-01-15 16:19:59 +0100

    update the stackage LTS version by nearly a year, 7.19->10.3. Need a git version of hsqml, need my own fork of th-printf as the maintainer is unresponsive. otherwise looking good.

--- install.sh
+++ install.sh
@@ -1,36 +1,39 @@
 command -v patch >/dev/null 2>&1 || { echo >&2 "I require the command 'patch' but it's not installed.  Aborting."; exit 1; }
 
-if [ ! -d "persistent-sqlcipher-2.6" ]; then
+if [ ! -d "persistent-sqlcipher-2.6.4" ]; then
     echo "setting up persistent sqlcipher integration..."
-    wget https://hackage.haskell.org/package/persistent-sqlite-2.6/persistent-sqlite-2.6.tar.gz
-    tar xvfz persistent-sqlite-2.6.tar.gz
-    rm persistent-sqlite-2.6.tar.gz
+    wget https://hackage.haskell.org/package/persistent-sqlite-2.6.4/persistent-sqlite-2.6.4.tar.gz
+    tar xvfz persistent-sqlite-2.6.4.tar.gz
+    rm persistent-sqlite-2.6.4.tar.gz
     patch -p0 <<'EOF'
---- persistent-sqlite-2.6/persistent-sqlite.cabal	2016-08-14 00:03:34.000000000 +0200
-+++ persistent-sqlite-2.6/persistent-sqlcipher.cabal	2017-02-11 10:53:17.086611649 +0100
+--- persistent-sqlite-2.6.4/persistent-sqlite.cabal	2017-12-02 22:27:44.000000000 +0100
++++ persistent-sqlite-2.6.4/persistent-sqlcipher.cabal	2018-01-04 18:15:27.309943866 +0100
 @@ -1,4 +1,4 @@
 -name:            persistent-sqlite
 +name:            persistent-sqlcipher
- version:         2.6
+ version:         2.6.4
  license:         MIT
  license-file:    LICENSE
-@@ -39,12 +39,7 @@
+@@ -44,15 +44,7 @@
      exposed-modules: Database.Sqlite
                       Database.Persist.Sqlite
      ghc-options:     -Wall
 -    if flag(systemlib)
--        extra-libraries: sqlite3
+-        if flag(use-pkgconfig)
+-            pkgconfig-depends: sqlite3
+-        else
+-            extra-libraries: sqlite3
 -    else
 -        c-sources:   cbits/sqlite3.c
 -        include-dirs: cbits
 -        cc-options:  -fPIC -std=c99
 +    extra-libraries: sqlcipher
-
+ 
      c-sources: cbits/config.c
 
 EOF
-    mv persistent-sqlite-2.6/ persistent-sqlcipher-2.6/
-    mv persistent-sqlcipher-2.6/persistent-sqlite.cabal persistent-sqlcipher-2.6/persistent-sqlcipher.cabal
+    mv persistent-sqlite-2.6.4/ persistent-sqlcipher-2.6.4/
+    mv persistent-sqlcipher-2.6.4/persistent-sqlite.cabal persistent-sqlcipher-2.6.4/persistent-sqlcipher.cabal
 fi
 echo "Will now install the haskell compiler if needed"
 stack setup
